f93e79caee7e8d5f497c69a20f2cb07f588e45b7
Rob Inglis <rob@workingsoftware.co.za>
2018-02-27 19:27:31 +0200

    Added login form

--- index.php
+++ index.php
@@ -1,1 +1,42 @@
-<? echo "Hello World"; ?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>User Login</title>
+
+    <!-- UIkit CSS -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/uikit/3.0.0-beta.40/css/uikit.min.css" />
+
+
+</head>
+
+<body>
+
+    <div class="uk-section uk-container uk-text-center">
+        <form class="js-login">
+            <fieldset class="uk-fieldset">
+
+                <legend class="uk-legend">Login</legend>
+
+                <div class="uk-margin">
+                    <input class="uk-input" type="email" required="required" placeholder="Email">
+                </div>
+                <div class="uk-margin">
+                    <input class="uk-input" type="password" required="required" placeholder="Password">
+                </div>
+                <div class="uk-margin">
+                <button class="uk-button uk-button-default" type="submit">Login</button>
+
+            </fieldset>
+        </form>
+    </div>
+
+    <!-- UIkit JS -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/uikit/3.0.0-beta.40/js/uikit.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/uikit/3.0.0-beta.40/js/uikit-icons.min.js"></script>
+</body>
+
+</html>
